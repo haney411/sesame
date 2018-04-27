9fa44d785fa6be80c7c123c30791b501abb27ad7
haney411 <37707495+haney411@users.noreply.github.com>
2018-04-27 16:28:55 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,7 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../sesame/'))
 
 # -- General configuration ------------------------------------------------
 
