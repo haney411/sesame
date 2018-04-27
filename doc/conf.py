516c9dddbe73bca19f449c7f56f0745cdee2181a
haney411 <37707495+haney411@users.noreply.github.com>
2018-04-27 17:09:01 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+autodoc_mock_imports = ['_tkinter']
 
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
